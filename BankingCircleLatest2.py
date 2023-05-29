4311ae957e4ef658e45889bbdcdafa5024fe60f9
Vladimir Bogomolov <99024007+bogomolov-vladimir@users.noreply.github.com>
2023-05-29 10:14:38 +0300

    Update BankingCircleLatest2.py

--- BankingCircleLatest2.py
+++ BankingCircleLatest2.py
@@ -5,12 +5,12 @@ import pandas as pd
 import os, json, time
 
 
-GENERAL_WEBHOOK_SECRET_DEV="**********"
+"""CORRECT RESULT"""
+
+GENERAL_WEBHOOK_SECRET_DEV="T3DS9S2HF/B03MZEDUYAF/ryrzAGxVofwLRm57FgyDTRQt"
 HOOK_URL = "https://hooks.slack.com/services/%s" % GENERAL_WEBHOOK_SECRET_DEV
 current_dateTime = str(datetime.now()).split(" ")[0].split("-")[2]
 nl = '\n'
-
-
     
 class BankingCircle:
 
@@ -18,28 +18,13 @@ class BankingCircle:
         self.link = link
         self.soup = BeautifulSoup(requests.get(self.link).text, 'html.parser')
         self.incident_detected = []
+        
      # save link for scraping, not nessesary to have it      
     def get_link(self):
         return self.link
+    
     #parse web page to detect if there was an incident at the moment
     def detect_incident(self):
-
-        def to_string(data):
-            return data.string
-        #making valid fotmat for Slack
-        def prepare_notification_for_slack(data):
-            slack_message = {'text': data, 'Attachment': "Notification! "}
-            req = requests.post(HOOK_URL, json.dumps(slack_message))
-
-        def sending_incident_notificarion(data):
-            if data != None:
-                #results = pd.DataFrame(data).T.rename(columns={0:"Status", 1:"Month", 2:"Date", 3:"Time", 4:"Year", 5:"Comments"})
-                results = data #this looks better in Slack
-                prepare_notification_for_slack(f"<!subteam**********> BankingCircle Incident Notification {nl}{results}")
-
-            else:
-                prepare_notification_for_slack(f"<!subteam**********> BankingCircle Incident Notification {nl} No Incidents reported today!")
-
         incident_content=self.soup.find_all("div", class_="status-day font-regular")
         incident_comments=incident_content[0].find_all("span", class_="whitespace-pre-wrap")
         incident_status=incident_content[0].find_all("strong")
@@ -48,8 +33,7 @@ class BankingCircle:
 
         for idx in range(len(incident_comments)):
             if to_string(date) == current_dateTime and len(incident_status) >= 1:
-            #if to_string(date) == "16" and len(incident_status) >= 1:
-            #creating varaibles per incident time updates
+                #creating varaibles per incident time updates
                 comments=[]
                 time=incident_content[0].find_all("small")[idx].find_all("var", "var-data"=="date")[1]
                 year=incident_content[0].find_all("div", class_="date border-color font-large")[0].find_all("var", "var-data"=="year")[1]
@@ -68,9 +52,20 @@ class BankingCircle:
                                      to_string(time),
                                      to_string(year),
                                      str(incident_comments[idx]).split("</br></span>")[0].replace("<br>", f"{nl}").split(">")[1]])
-                    
                 sending_incident_notificarion(comments[0])
 
+def to_string(data):
+    return data.string
+
+#making valid fotmat for Slack
+def prepare_notification_for_slack(data):
+    slack_message = {'text': data, 'Attachment': "Notification! "}
+    req = requests.post(HOOK_URL, json.dumps(slack_message))
+
+def sending_incident_notificarion(data):
+    if data != None:
+        prepare_notification_for_slack(f"<!subteam^S04QLK720LC> BankingCircle Incident Notification {nl}{data}")           
+                              
 def job():
    # executing the task every 30 seconds
     BankingCircle('https://bankingcircleconnect.statuspage.io/#past-incidents').detect_incident()
